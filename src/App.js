5ad2b55bca363b1d8591cfd82a6021169162dd73
Vasusingh123 <146229653+Vasusingh123@users.noreply.github.com>
2024-03-23 18:12:15 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,67 @@
-import logo from './logo.svg';
+import TextForm from './components/TextForm'
 import './App.css';
-
+import Navbar from './components/Navbar';
+import Alert from './components/Alert';
+import { useState } from 'react';
+import React from 'react'
+//import About from './components/About';
+//import { BrowserRouter as Router, Switch, Route, } from "react-router-dom";
 function App() {
+   const [mode,setmode]=useState('light');
+   const [alert,setalert] = useState(null);
+   const showalert=(msg,type)=>{
+    setalert({
+      msg : msg,
+      type : type
+    })
+    setTimeout(() => {
+      setalert(null);
+    }, 2000);
+   }
+   const togglemode=()=>{
+    if(mode==='light')
+    {
+      setmode('dark');
+      document.body.style.backgroundColor='#042743';
+      showalert("Dark mode has been enabled","success");
+      document.title='TextUtils - Dark Mode';
+    }
+    else{
+      setmode('light');
+      document.body.style.backgroundColor='white';
+      showalert("light mode has been enabled","success");
+      document.title='TextUtils - Light Mode';
+    }
+   }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <React.Fragment> 
+   {/*<Router>*/}
+   <Navbar  mode={mode} togglemode={togglemode}/>
+   <Alert alert={alert}/>
+  
+    
+    
+    {/*<Switch>
+      
+       <Route exact path="/about">
+        <About/>
+       </Route>
+ 
+       <Route exact path="/">
+        */}
+       <div className="container my-3">
+       <TextForm showalert={showalert} mode={mode} heading="Enter the text to analyze"/>
+       </div>
+      {/* </Route>
+      
+    </Switch>
+    
+      </Router>*/}
+    
+  
+   
+   
+   </React.Fragment>
   );
 }
 
